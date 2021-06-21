864ea47459aada433d68fc7f6332b9d3ab6a0c28
Dariusz Marchewka <dariusz.marchewka@momox.biz>
2021-06-21 21:17:11 +0200

    Initial commit

---
+++ api/v1.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from pydantic.types import UUID
+
+from core.schemas import Route
+
+router = APIRouter(
+    prefix="/route",
+    tags=["route"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/", status_code=201)
+def create_route(route: Route):
+    print('test')
+    return route
+
+
+@router.post('/{route_id}/way_point/', status_code=201)
+def add_way_point(route_id: UUID):
+    # coordinates = request.get_json()  # {“lat”: 59.23425, “lon”: 18.23526}
+    # assert 'lat' in coordinates
+    # assert 'lon' in coordinates
+    return {}
+
+
+@router.get('/{route_id}/length/')
+def calculate_length(route_id: UUID):
+    return {}
---
+++ main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+from api.v1 import router
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+app = FastAPI()
+app.include_router(router)
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
