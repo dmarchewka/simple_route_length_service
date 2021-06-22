de051ca8cac6710ff64ab9563de5fa26773984f7
Dariusz Marchewka <dariusz.marchewka@momox.biz>
2021-06-22 18:31:42 +0200

    Add DB mock

--- core/database.py
+++ core/database.py
@@ -1,1 +1,13 @@
-routes = {}
+routes_db = None
+
+
+def get_routes_db():
+    global routes_db
+    if not routes_db:
+        routes_db = {}
+    return routes_db
+
+
+def reset_routes_db():
+    global routes_db
+    routes_db = {}
